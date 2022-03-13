b89fa49d4f82d6a401cc18334afc886f93056dc3
Michael Taylor <michael.t74x@gmail.com>
2022-03-13 16:09:58 +0000

    Initial commit 3

--- CMP1903M Assessment 1 Base Code/Program.cs
+++ CMP1903M Assessment 1 Base Code/Program.cs
@@ -30,7 +30,7 @@ namespace CMP1903M_Assessment_1_Base_Code
 
             //TO ADD: Get the frequency of individual letters?
 
-           Console.WriteLine("Hello");
+           
         }
         
         
