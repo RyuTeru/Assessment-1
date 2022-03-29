f2dbb0db148e7cd5b63fd868d41b2c79fa76a966
Michael Taylor <michael.t74x@gmail.com>
2022-03-29 20:27:16 +0100

    LongWordFile class finished

--- CMP1903M Assessment 1 Base Code/Analyse.cs
+++ CMP1903M Assessment 1 Base Code/Analyse.cs
@@ -93,37 +93,7 @@ public class Analyse
 
             return values;
         }
-        // Method to find all words longer than 7 letters 
-        public void findLongWords(string input)
-        {   
-            // Creation of list of long words
-            List<string> longWords = new List<string>();
-            // Temporary string to hold words
-            string word = "";
-            foreach (char character in input.ToCharArray())
-            {  
-                // If a real character is found then adds it to the temp string
-                if (char.IsLetterOrDigit(character))
-                {
-                   word += character.ToString();
-                   continue;
-                }
-                // If a non-character is found then checks the length of current temp string 
-                if (word.Length > longWordCap)
-                {
-                    longWords.Add(word);
-                }
-                word = "";
-            }
-            // Creation of set to gather the unique long words
-            var longWordSet = longWords.Distinct();
-            string currentDir = Environment.CurrentDirectory;
-            using (StreamWriter outputFile = new StreamWriter(Path.Combine(currentDir, "long words.txt")))
-            {
-                foreach (string words in longWordSet)
-                    outputFile.WriteLine(words);
-            }
-        }   
+        
         // Method to find the frequency of all letters 
         public void letterFrequency(string input)
         {
